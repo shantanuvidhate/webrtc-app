94bd9f292afaf35d9276a408098e1df5ac7848e7
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-11-11 10:33:06 +0530

    temp file upload for static content

---
+++ components/file-upload.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+interface FileUploadProps {
+    onChange : (url?:string) => void;
+    value : string;
+}
+
+export const FileUpload=({onChange,value}:FileUploadProps)=>{
+    return (
+        <></>
+    )
+}
