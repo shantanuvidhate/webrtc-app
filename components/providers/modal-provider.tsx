8b7c79812271106da4d3428ab23aa18100c253a6
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-11-12 22:49:09 +0530

    invite modal in Modal Provider

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,18 +2,22 @@
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "@/components/modals/invite-modal";
 
-export const ModalProvider =() =>{
-    const [isMounted , setIsMounted] = useState(false);
+export const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         setIsMounted(true);
-    },[]);
+    }, []);
 
     if (!isMounted) {
         return null;
     }
     return (
-        <CreateServerModal />
+        <>
+            <CreateServerModal />
+            <InviteModal />
+        </>
     );
 }
