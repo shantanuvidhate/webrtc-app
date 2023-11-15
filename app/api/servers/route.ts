030a04bf7cb1d034662f83121bf53c74e10fd941
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-11-16 01:44:28 +0530

    Payload change

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -4,38 +4,37 @@ import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
-export async function POST (req: Request) {
-    try {
-        const {values:{name}, imageUrl} = await req.json();
-        // const {name , imageUrl} = await req.json();
-        const profile = await currentProfile();
+export async function POST(req: Request) {
+  try {
+    const { name, imageUrl } = await req.json();
+    const profile = await currentProfile();
 
-        if (!profile) {
-            return new NextResponse("Unauthorized" , {status : 401});
-        }
-        const server = await db.server.create({
-            data : {
-                profileId : profile.id,
-                name,
-                imageUrl,  // need to upload edgestore image url
-                inviteCode: uuidv4(),
-                channels : {
-                    create : [
-                        {name : "general" , profileId: profile.id}
-                    ]
-                },
-                members : {
-                    create : [
-                        {profileId : profile.id , role : MemberRole.ADMIN} 
-                    ]
-                }
-            }
-        });
-        return NextResponse.json(server);
-
-
-    }catch (error) {
-        console.log("[SERVER_POST]", error);
-        return new NextResponse("Internal server error" , {status : 500});
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const server = await db.server.create({
+      data : {
+        profileId : profile.id,
+        name,
+        imageUrl,
+        inviteCode: uuidv4(),
+        channels : {
+          create : [
+            { name : "general", profileId: profile.id }
+          ]
+        },
+        members: {
+          create: [
+            { profileId: profile.id, role: MemberRole.ADMIN }
+          ]
+        }
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVERS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
