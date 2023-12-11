7e7f8e47d3ef37388390f1c8eb817440ad4e7bb6
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-12-11 17:44:56 +0530

    onClick routing to channels from search component

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -3,6 +3,7 @@
 import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
     data: {
@@ -19,23 +20,37 @@ interface ServerSearchProps {
 export const ServerSearch = ({ data }: ServerSearchProps) => {
 
     const [open, setOpen] = useState(false);
+    const router = useRouter();
+    const params = useParams();
 
-    useEffect(()=>{
-        const down = (event:KeyboardEvent)=>{
+    useEffect(() => {
+        const down = (event: KeyboardEvent) => {
             if (event.key == "k" && (event.metaKey || event.ctrlKey)) {
                 event.preventDefault();
-                setOpen((open)=>!open);
+                setOpen((open) => !open);
             }
         }
 
-        document.addEventListener("keydown" , down);
+        document.addEventListener("keydown", down);
 
-        return ()=> document.removeEventListener("keydown" , down);
-    },[]);
-    
+        return () => document.removeEventListener("keydown", down);
+    }, []);
+
+    const onClick = ({ id, type }: { id: string, type: "channel" | "member" }) => {
+        setOpen(false);
+
+        if (type == "member") {
+            return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+        }
+
+        if (type == "channel") {
+            return router.push(`/servers/${params?.serverId}/channels/${id}`);
+        }
+    }
+
     return (
         <>
-            <button onClick={()=>setOpen(true)} className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+            <button onClick={() => setOpen(true)} className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
                 <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
                 <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">Search</p>
                 <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
@@ -43,18 +58,18 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
                 </kbd>
             </button>
             <CommandDialog open={open} onOpenChange={setOpen}>
-                <CommandInput placeholder="Search all channels and members"/>
+                <CommandInput placeholder="Search all channels and members" />
                 <CommandList>
                     <CommandEmpty>
                         No Results Found
                     </CommandEmpty>
-                    {data.map(({label , type , data})=>{
+                    {data.map(({ label, type, data }) => {
                         if (!data?.length) return null;
                         return (
                             <CommandGroup key={label} heading={label}>
-                                {data?.map(({id,icon,name})=>{
+                                {data?.map(({ id, icon, name }) => {
                                     return (
-                                        <CommandItem key={id}>
+                                        <CommandItem key={id} onSelect={() => onClick({ id, type })}>
                                             {icon}
                                             <span>{name}</span>
                                         </CommandItem>
