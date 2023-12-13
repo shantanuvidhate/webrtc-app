d543fa4e3656dc37a7000799e3dc9d2b49ffc02f
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-12-13 23:48:03 +0530

    PATCH API for editchannel modal

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -24,23 +24,23 @@ export async function DELETE(req: Request, { params }: { params: { channelId: st
         }
 
         const server = await db.server.update({
-            where : {
+            where: {
                 id: serverId,
-                members : {
-                    some : {
-                        profileId : profile.id,
-                        role : {
-                            in : [MemberRole.ADMIN , MemberRole.MODERATOR]
+                members: {
+                    some: {
+                        profileId: profile.id,
+                        role: {
+                            in: [MemberRole.ADMIN, MemberRole.MODERATOR]
                         }
                     }
                 }
             },
-            data : {
-                channels : {
-                    delete : {
-                        id : params.channelId,
-                        name : {
-                            not : "general"
+            data: {
+                channels: {
+                    delete: {
+                        id: params.channelId,
+                        name: {
+                            not: "general"
                         }
                     }
                 }
@@ -51,6 +51,69 @@ export async function DELETE(req: Request, { params }: { params: { channelId: st
 
     } catch (error) {
         console.log("[CHANNEL_ID_DELETE]", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
+
+export async function PATCH(req: Request, { params }: { params: { channelId: string } }) {
+    try {
+
+        const profile = await currentProfile();
+        const { searchParams } = new URL(req.url);
+        const { name, type } = await req.json();
+
+        const serverId = searchParams.get("serverId");
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!serverId) {
+            return new NextResponse("Server ID missing", { status: 400 });
+        }
+
+        if (!params.channelId) {
+            return new NextResponse("Channel ID missing", { status: 400 });
+        }
+        // restrict the bypassing the fronted validation
+        if (name === "general") {
+            return new NextResponse("Name cannot be 'general'", { status: 400 });
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                members: {
+                    some: {
+                        profileId: profile.id,
+                        role: {
+                            in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+                        }
+                    }
+                }
+            },
+            data: {
+                channels: {
+                    update: {
+                        where : {
+                            id : params.channelId,
+                            NOT : {
+                                name : "general"
+                            }
+                        },
+                        data : {
+                            name,
+                            type
+                        }
+                    }
+                }
+            }
+        });
+
+        return NextResponse.json(server);
+
+    } catch (error) {
+        console.log("[CHANNEL_ID_PATCH]", error);
         return new NextResponse("Internal Server Error", { status: 500 });
     }
 }
