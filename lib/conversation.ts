f873f754b9a37ff84a0b6960ddba332dc199b8c3
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-12-14 16:14:18 +0530

    conversation lib

---
+++ lib/conversation.ts
@@ -0,0 +1,65 @@
+import { db } from "@/lib/db";
+
+export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
+    let conversation = await findConversation(memberOneId, memberTwoId) || await findConversation(memberTwoId, memberOneId);
+
+    // If I do not find any conversation then create new conversation
+    if (!conversation) {
+        conversation = await createNewConversations(memberOneId, memberTwoId);
+    }
+
+    return conversation;
+}
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+    try {
+        return await db.conversation.findFirst({
+            where: {
+                AND: [
+                    { memberOneId: memberOneId },
+                    { memberTwoId: memberTwoId }
+                ]
+            },
+            include: {
+                memberOne: {
+                    include: {
+                        profile: true
+                    }
+                },
+                memberTwo: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+        });
+    } catch {
+        return null;
+    }
+}
+
+const createNewConversations = async (memberOneId: string, memberTwoId: string) => {
+    try {
+        return await db.conversation.create({
+            data: {
+                memberOneId,
+                memberTwoId
+            },
+            include: {
+                memberOne: {
+                    include: {
+                        profile: true
+                    }
+                },
+                memberTwo: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+        })
+    }
+    catch {
+        return null;
+    }
+}
