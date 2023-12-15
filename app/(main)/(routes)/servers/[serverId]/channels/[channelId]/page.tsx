59cddd018a73f52aa1c36b5ea935a7c7037bd718
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-12-15 10:42:45 +0530

    channel Id page

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -19,24 +20,29 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     }
 
     const channel = await db.channel.findUnique({
-        where : {
-            id : params.channelId
+        where: {
+            id: params.channelId
         }
     });
 
     const member = await db.member.findFirst({
-        where : {
+        where: {
             serverId: params.serverId,
             profileId: profile.id
         }
     });
 
     if (!channel || !member) {
-        redirect ("/");
+        redirect("/");
     }
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+            <div className="flex-1"> Future Messages</div>
+            <ChatInput name={channel.name} type="channel" apiUrl="/api/socket/messages" query={{
+                channelId: channel.id,
+                serverId: channel.serverId
+            }} />
         </div>
     );
 }
