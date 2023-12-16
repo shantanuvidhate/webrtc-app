2d0e7e0586a0eb9ce636d8dde8cb149babe172f3
shantanuvidhate <shantanuvidhate00@gmail.com>
2023-12-16 14:18:07 +0530

    Loader added

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,6 +2,8 @@
 
 import { Member } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
 
 interface ChatMessagesProps {
     name: string;
@@ -15,10 +17,35 @@ interface ChatMessagesProps {
     type: "channel" | "conversation";
 }
 export const ChatMessages = ({ apiUrl, chatId, member, name, paramKey, paramValue, socketQuery, socketUrl, type }: ChatMessagesProps) => {
+    const queryKey = `chat:${chatId}`;
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({
+        queryKey,
+        apiUrl,
+        paramKey,
+        paramValue
+    });
+
+    if (status === "pending") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin"/>
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading messages...</p>
+            </div>
+        )
+    }
+
+    if (status === "error") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <ServerCrash className="h-7 w-7 text-zinc-500"/>
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">Something went wrong!</p>
+            </div>
+        )
+    }
     return (
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto"> 
-        <div className="flex-1"/>
-        <ChatWelcome type={type} name={name} />
+        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+            <div className="flex-1" />
+            <ChatWelcome type={type} name={name} />
         </div>
     );
 }
